22bf0a107f04efc6523813942a6594ac95d3c8fb
Zoltai Balázs <zoltai.balazs@students.jedlik.eu>
2023-03-06 09:36:56 +0100

    Removed preset button hover highlight

--- Rajzi/Rajzi/MainWindow.xaml.cs
+++ Rajzi/Rajzi/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Rajzi
     /// </summary>
     public partial class MainWindow : Window
     {
+        Button activeBtn;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +31,48 @@ namespace Rajzi
         {
             RunWindow newWindow = new RunWindow();
             newWindow.Show();
+        }
+
+        private void IsActive(object sender, RoutedEventArgs e)
+        {
+            BrushConverter bc = new BrushConverter();
+            if (activeBtn != null)
+            {
+                activeBtn.Background = (Brush)bc.ConvertFrom("#2F2235");
+                activeBtn.Width = 110;
+                if (activeBtn == sender)
+                {
+                    activeBtn.Background = (Brush)bc.ConvertFrom("#2F2235");
+                    activeBtn.Width = 110;
+                    activeBtn = null;
+                    grMainGrid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
+                    grMainGrid.ColumnDefinitions[1].Width = new GridLength(0, GridUnitType.Star);
+                    grMainGrid.ColumnDefinitions[2].Width = new GridLength(5, GridUnitType.Star);
+                }
+                else
+                {
+                    activeBtn = sender as Button;
+                    activeBtn.Background = (Brush)bc.ConvertFrom("#EB9486");
+                    activeBtn.Width = 128;
+                    grMainGrid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
+                    grMainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
+                    grMainGrid.ColumnDefinitions[2].Width = new GridLength(4, GridUnitType.Star);;
+                }
+            }
+            else
+            {
+                activeBtn = sender as Button;
+                activeBtn.Background = (Brush)bc.ConvertFrom("#EB9486");
+                activeBtn.Width = 128;
+                grMainGrid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
+                grMainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
+                grMainGrid.ColumnDefinitions[2].Width = new GridLength(4, GridUnitType.Star);
+            }
+        }
+
+        private void MenuOpt(object sender, RoutedEventArgs e)
+        {
+            IsActive(sender, e);
         }
     }
 }
