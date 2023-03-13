9d9fd2dce9c2f38037117a72e7ab1e71bac58c65
Aron Aron <teknosvizilo@gmail.com>
2023-03-13 08:39:51 +0100

    PNG-save

--- Rajzi/Rajzi/RunWindow.xaml.cs
+++ Rajzi/Rajzi/RunWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using static System.Net.Mime.MediaTypeNames;
 using System.Windows.Media;
 using System.Runtime.Intrinsics;
+using System.IO;
 
 namespace Rajzi
 {
@@ -33,6 +34,14 @@ namespace Rajzi
             Point endPoint2 = new Point(4, 2);
             var metszet = vectorIntersection(startPoint, endPoint, startPoint2, endPoint2);
             teszt3.Content = metszet;
+            forward(100);
+            pencil.changeRotate(90);
+            forward(100);
+            pencil.changeRotate(180);
+            forward(100);
+            pencil.changeRotate(270);
+            forward(100);
+            pencil.changeRotate(0);
         }
         private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
             {
@@ -108,6 +117,63 @@ namespace Rajzi
             {
                 return Tuple.Create(double.NaN, double.NaN);
             }
+        }
+
+
+
+        public static class RenderVisualService
+        {
+            private const double defaultDpi = 96.0;
+
+            public static void RenderToPNGFile(Visual targetControl, string filename)
+            {
+                var renderTargetBitmap = GetRenderTargetBitmapFromControl(targetControl);
+
+                var encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+                var result = new BitmapImage();
+
+                try
+                {
+                    using (var fileStream = new FileStream(filename, FileMode.Create))
+                    {
+                        encoder.Save(fileStream);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Hiba volt a fájl mentésében: {ex.Message}");
+                }
+            }
+
+            private static BitmapSource GetRenderTargetBitmapFromControl(Visual targetControl, double dpi = defaultDpi)
+            {
+                if (targetControl == null) return null;
+
+                var bounds = VisualTreeHelper.GetDescendantBounds(targetControl);
+                var renderTargetBitmap = new RenderTargetBitmap((int)(bounds.Width * dpi / 96.0),
+                                                                (int)(bounds.Height * dpi / 96.0),
+                                                                dpi,
+                                                                dpi,
+                                                                PixelFormats.Pbgra32);
+
+                var drawingVisual = new DrawingVisual();
+
+                using (var drawingContext = drawingVisual.RenderOpen())
+                {
+                    var visualBrush = new VisualBrush(targetControl);
+                    drawingContext.DrawRectangle(visualBrush, null, new Rect(new Point(), bounds.Size));
+                }
+
+                renderTargetBitmap.Render(drawingVisual);
+                return renderTargetBitmap;
+            }
+        }
+
+        private void PngSave(object sender, RoutedEventArgs e)
+        {
+            RenderVisualService.RenderToPNGFile(Canvas, $"{FileName.Text}.png");
         }
     }
 }
